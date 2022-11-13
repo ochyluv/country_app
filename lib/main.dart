44cd412d22a5e055a721f976bc29ca1164c2afc6
Ochanya Queen Ameh <queenochanya58@gmail.com>
2022-11-13 19:10:17 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
-import 'package:country_app/views/startup/startup_view.dart';
+import 'package:country_app/app/app.locator.dart';
+import 'package:country_app/app/app.router.dart';
 import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupLocator();
+
   runApp(const MyApp());
 }
 
@@ -11,10 +16,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'My Country App',
-      home: StartUpView(),
+      initialRoute: Routes.startUpView,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      navigatorKey: StackedService.navigatorKey,
     );
   }
 }
