60f6a813deaeab1a67b5431202724a7191b8bd6a
Ochanya Queen Ameh <queenochanya58@gmail.com>
2022-11-13 19:07:14 +0100

    Create edge_insets.dart

---
+++ lib/core/constants/edge_insets.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+EdgeInsetsGeometry get kEdgeInsetsZero => EdgeInsets.zero;
+
+EdgeInsetsGeometry get kEdgeInsetsHorizontalTiny =>
+    const EdgeInsets.symmetric(horizontal: 8.0);
+
+EdgeInsetsGeometry get kEdgeInsetsHorizontalSmall =>
+    const EdgeInsets.symmetric(horizontal: 15.0);
+
+EdgeInsetsGeometry get kEdgeInsetsHorizontalNormal =>
+    const EdgeInsets.symmetric(horizontal: 24.0);
+
+EdgeInsetsGeometry get kEdgeInsetsVerticalSmall =>
+    const EdgeInsets.symmetric(vertical: 8.0);
+
+EdgeInsetsGeometry get kEdgeInsetsVerticalNormal =>
+    const EdgeInsets.symmetric(vertical: 12.0);
+
+EdgeInsetsGeometry get kEdgeInsetsAllSmall => const EdgeInsets.all(6.0);
+
+EdgeInsetsGeometry get kEdgeInsetsAllNormal => const EdgeInsets.all(12.0);
+
+EdgeInsetsGeometry get kEdgeInsetsAllLarge => const EdgeInsets.all(24.0);
