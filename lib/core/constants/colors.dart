94d2d7d04f8f9afaba58a832a68d0469b97077f6
Ochanya Queen Ameh <queenochanya58@gmail.com>
2022-11-13 19:10:10 +0100

    Create colors.dart

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kWhiteColor = Color(0xFFFFFFFF);
+const Color kBlackColor = Color(0xFF000000);
+const Color kBlueColor = Color(0xFF001637);
+const Color kGreyColor = Color(0xFFF2F4F7);
+const Color kPinkColor = Color.fromARGB(255, 240, 186, 216);
