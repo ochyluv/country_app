19e1b11a4231ad6236a76bb92536d244e0366def
Ochanya Queen Ameh <queenochanya58@gmail.com>
2022-11-14 12:25:08 +0100

    Update details_row.dart

--- lib/ui/widgets/details_row.dart
+++ lib/ui/widgets/details_row.dart
@@ -1,16 +1,13 @@
 import 'package:country_app/core/constants/text_styles.dart';
-import 'package:country_app/core/models/country_model.dart';
 import 'package:flutter/material.dart';
 
 class DetailsRow extends StatelessWidget {
   const DetailsRow({
     Key? key,
-    required this.country,
     required this.title,
     required this.details,
   }) : super(key: key);
 
-  final CountryModel country;
   final String title;
   final String details;
 
@@ -21,7 +18,10 @@ class DetailsRow extends StatelessWidget {
         text: title,
         style: kSubtitleBoldTextStyle,
         children: [
-          TextSpan(text: details, style: kSubtitleRegularTextStyle),
+          TextSpan(
+            text: details,
+            style: kSubtitleRegularTextStyle,
+          ),
         ],
       ),
     );
